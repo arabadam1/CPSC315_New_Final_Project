4ce6095211bfa06b41218bd75d75fccef782a8f3
dillonshipley <dshipley@zagmail.gonzaga.edu>
2020-11-27 16:56:59 -0800

    Added all views

---
+++ FitBeats/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StartViewController.swift
+//  FitBeats
+//
+//  Created by Dillon Shipley on 11/27/20.
+//
+
+import Foundation
+import UIKit
+
+class BeginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
