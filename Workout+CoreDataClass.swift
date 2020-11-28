58aef0a566cee7075b074acffcf6a780e13ca3e5
Adrian Rabadam <arabadam@zagmail.gonzaga.edu>
2020-11-27 16:55:53 -0800

    Core data implemented

---
+++ Workout+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Workout+CoreDataClass.swift
+//  FitBeats
+//
+//  Created by Adrian Rabadam on 11/27/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Workout)
+public class Workout: NSManagedObject {
+
+}
